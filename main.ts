0b8565ecf705aca64e55fbded2a97ba49bec7884
elisabreness <135571243+elisabreness@users.noreply.github.com>
2023-06-04 23:37:46 -0600

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    if (input.acceleration(Dimension.Y) < -20) {
+    if (input.acceleration(Dimension.X) < -20) {
         basic.showString("<")
     }
     if (input.acceleration(Dimension.X) > 20) {
